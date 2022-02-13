c237bb138d15f8ec872cf631456ef09ce6c572ff
Robert Sainton <robertsainton@outlook.com>
2022-02-13 00:46:54 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return string.toLowerCase();
+  }
+function logShout(string) {
+    console.log("HELLO");
+    return string;
+  }
+function logWhisper(string) {
+    console.log("hello");
+    return string;
+  }
+
+function sayHiToHeadphonedRoommate(string){
+        console.log(string);
+        console.log(string.toLowerCase());
+        console.log(string.toUpperCase());    
+     if (string === string.toLowerCase()){ return "I can't hear you!";}    
+      else if(string === string.toUpperCase()) {return "YES INDEED!";} 
+      else return "I would love to!";
+      
+}
+    
